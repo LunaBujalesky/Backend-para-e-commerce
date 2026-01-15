e27d9972269bc7e34d4dc14c218a3a60455a32f5
Luna Bujalesky <lunitabruja@gmail.com>
2026-01-15 17:10:15 -0300

    datos en las vistas que faltaban

--- src/models/Product.model.js
+++ src/models/Product.model.js
@@ -1,17 +1,21 @@
 import mongoose from "mongoose";
 import mongoosePaginate from "mongoose-paginate-v2";
 
-const productSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: String,
-  code: { type: String, required: true },
-  price: { type: Number, required: true },
-  status: { type: Boolean, default: true },
-  stock: Number,
-  category: String,
-  thumbnails: [String]
-});
-
+const productSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: String,
+    code: { type: String, required: true },
+    price: { type: Number, required: true },
+    status: { type: Boolean, default: true },
+    stock: Number,
+    category: String,
+    thumbnails: [String]
+  },
+  {
+    timestamps: true
+  }
+);
 productSchema.plugin(mongoosePaginate);
 
 export const ProductModel = mongoose.model("Product", productSchema);
